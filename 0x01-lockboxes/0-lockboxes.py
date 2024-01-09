e42d6edc1d46ba36f84fdc915df8636d403819df
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-09 12:56:24 +0000

    Task 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+""" Write a method that determines if all the boxes can be opened
+"""
+
+
+def canUnlockAll(boxes):
+    """ Can Unlock All
+
+        Args:
+            boxes: a list of lists
+    """
+
+    boxes_len = len(boxes)
+
+    for i in range(1, boxes_len):
+        unlocked = False
+        for j in range(boxes_len):
+            if j != i and i in boxes[j]:
+                unlocked = True
+                break
+
+    return (unlocked)
