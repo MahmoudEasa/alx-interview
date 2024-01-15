17dae35c38a1ac14ad451f97942e85e099e7480b
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-15 15:32:45 +0000

    Task 0-minoperations.py

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -10,3 +10,18 @@ print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
 n = 12
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 6
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 20
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 1
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 15
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 25
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
