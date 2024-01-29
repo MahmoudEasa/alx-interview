ed844cca123e387ef83f81dd048f2df1d938184a
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-29 08:56:35 +0000

    Solve Task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Write a method that determines if a given data set
+    represents a valid UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    """ Method that determines if a given data
+        set represents a valid UTF-8 encoding
+    """
+    data_has_len = False
+
+    if not isinstance(data, list): return (False)
+
+    for item in data:
+        data_has_len = True
+        if item < 0 or item > 255: return (False)
+
+
+    return (data_has_len)
+
