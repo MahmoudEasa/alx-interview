06d06cfa4696627fc2cb8d5d1a4b1ac2718fdc6a
MahmoudEasa <MahmoudEasa@gmail.com>
2024-02-01 02:30:49 +0000

    Solve Task 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,13 +8,21 @@ def validUTF8(data):
     """ Method that determines if a given data
         set represents a valid UTF-8 encoding
     """
-    data_has_len = False
-
-    if not isinstance(data, list): return (False)
+    n_bytes = 0
 
     for item in data:
-        data_has_len = True
-        if item < 0 or item > 255: return (False)
+        if n_bytes == 0:
+            if (item >> 5) == 0b110:
+                n_bytes = 1
+            elif (item >> 4) == 0b1110:
+                n_bytes = 2
+            elif (item >> 3) == 0b11110:
+                n_bytes = 3
+            elif (item >> 7) != 0:
+                return (False)
+        else:
+            if (item >> 6) != 0b10:
+                return (False)
+            n_bytes -= 1
 
-
-    return (data_has_len)
+    return (n_bytes == 0)
