ef32b181409572896453dab3ee4bb69d6cc239dd
MahmoudEasa <mu01011422865@gmail.com>
2024-02-13 02:12:13 +0200

    Solve Task Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+const request = require("request");
+const args = process.argv;
+
+request(
+	`https://swapi-api.alx-tools.com/api/films/${args[2]}`,
+	(error, response, body) => {
+		if (!error && response.statusCode === 200) {
+			const characters = JSON.parse(body)["characters"];
+
+			for (const ch of characters) {
+				request(ch, (err, res, data) => {
+					if (!err && res.statusCode === 200) {
+						console.log(JSON.parse(data)["name"]);
+					} else {
+						console.error("Error:", err);
+					}
+				});
+			}
+		} else {
+			console.error("Error:", error);
+		}
+	}
+);
