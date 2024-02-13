f3ec71b7a8d02678ef977cdf1964f6347e1c89ee
MahmoudEasa <mu01011422865@gmail.com>
2024-02-13 15:58:39 +0200

    Fix Task Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,24 +2,36 @@
 const request = require('request');
 const args = process.argv;
 
+const fetchCharacter = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, data) => {
+      if (!err && res.statusCode === 200) {
+        const character = JSON.parse(data);
+        resolve(character.name);
+      } else {
+        reject(err);
+      }
+    });
+  });
+};
+
 if (args.length === 3) {
   request(
 `https://swapi-api.alx-tools.com/api/films/${args[2]}`,
-(error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const characters = JSON.parse(body).characters;
+(err, res, data) => {
+  if (!err && res.statusCode === 200) {
+    const characters = JSON.parse(data).characters;
 
-    for (const ch of characters) {
-      request(ch, (err, res, data) => {
-        if (!err && res.statusCode === 200) {
-          console.log(JSON.parse(data).name);
-        } else {
-          console.error('Error:', err);
-        }
-      });
-    }
+    const fetchPromises = characters.map((url) =>
+      fetchCharacter(url)
+    );
+    Promise.all(fetchPromises)
+      .then((names) => {
+        names.forEach((name) => console.log(name));
+      })
+      .catch((err) => console.error('Error:', err));
   } else {
-    console.error('Error:', error);
+    console.error('Error:', err);
   }
 }
   );
