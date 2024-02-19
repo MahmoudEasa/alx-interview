240a2c0eef8a75031d10fa1d76b1061ca406334b
MahmoudEasa <mu01011422865@gmail.com>
2024-02-19 20:43:44 +0200

    Solve Rotate 2D Matrix Problem

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Rotate 2D Matrix
+    Given an n x n 2D matrix, rotate it 90 degrees clockwise.
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """ Rotate 2D Matrix """
+    matrix_len = len(matrix)
+    m_len = (matrix_len - 1)
+    i = m_len
+    c = 0
+    start = 0
+    while m_len > 1:
+        while c < m_len:
+            # tmp = matrix[0][c]
+            # matrix[0][c] = matrix[c][m_len]
+            # matrix[c][m_len] = matrix[m_len][i]
+            # matrix[m_len][i] = matrix[i][0]
+            # matrix[i][0] = tmp
+            tmp = matrix[start][i]
+            matrix[start][i] = matrix[c][start]
+            matrix[c][start] = matrix[m_len][c]
+            matrix[m_len][c] = matrix[i][m_len]
+            matrix[i][m_len] = tmp
+            c += 1
+            i -= 1
+        m_len = (m_len + 1) // 2
+        start += 1
+        c = start
+        i = m_len
