903ac47aff23bf28890478e1448d2f3d87ec36fc
MahmoudEasa <mu01011422865@gmail.com>
2024-03-05 14:25:06 +0200

    Fix Task island_perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,18 +6,20 @@
 def island_perimeter(grid):
     """ Returns the perimeter of the island described in grid """
     perimeter = 0
-    row_len = len(grid)
-    col_len = len(grid[0])
 
-    for row in range(1, row_len - 1):
-        for col in range(1, col_len - 1):
-            if grid[row][col]:
-                if not grid[row - 1][col]:
-                    perimeter += 1
-                if not grid[row + 1][col]:
-                    perimeter += 1
-                if not grid[row][col - 1]:
-                    perimeter += 1
-                if not grid[row][col + 1]:
-                    perimeter += 1
+    if isinstance(grid, list):
+        row_len = len(grid)
+        col_len = len(grid[0])
+        if all(isinstance(grid[i], list) for i in range(row_len)):
+            for row in range(1, row_len - 1):
+                for col in range(1, col_len - 1):
+                    if grid[row][col]:
+                        if not grid[row - 1][col]:
+                            perimeter += 1
+                        if not grid[row + 1][col]:
+                            perimeter += 1
+                        if not grid[row][col - 1]:
+                            perimeter += 1
+                        if not grid[row][col + 1]:
+                            perimeter += 1
     return (perimeter)
